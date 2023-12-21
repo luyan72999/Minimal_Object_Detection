313486c773832f66585427c6363bd4e3fdfe0fe0
Benjamin Kiefer <ben93_kiefer@yahoo.de>
2023-12-21 02:23:28 +0100

    Update training_dataset.py

--- training_dataset.py
+++ training_dataset.py
@@ -1,21 +1,19 @@
 import numpy as np
 import pandas as pd
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
+
+
 
 class training_dataset(Dataset):
     def __init__(self):
         trainX = np.asarray(pd.read_csv('./Dataset/trainingData.csv', sep=',', header=None))
         trainY = np.asarray(pd.read_csv('./Dataset/ground-truth.csv', sep=',', header=None))
-        self.features_train = torch.Tensor(trainX)
-        self.groundTruth_train = torch.Tensor(trainY)
+
+        self.features_train = torch.Tensor(trainX / 255.0) # Scaling images
+        self.groundTruth_train = torch.Tensor(trainY / 100.0)  # Scaling coordinates
 
         self.len = len(trainX)
-
-        # testX = np.asarray(pd.read_csv('./Dataset/testData.csv', sep=',', header=None))
-        # testY = np.asarray(pd.read_csv('./Dataset/ground-truth-test.csv', sep=',', header=None))
-        # self.features_test = torch.Tensor(testX)
-        # self.groundTruth_test = torch.Tensor(testY)
 
     def __getitem__(self, item):
         return self.features_train[item], self.groundTruth_train[item]
